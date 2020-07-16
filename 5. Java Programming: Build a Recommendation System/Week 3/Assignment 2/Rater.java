3c2af735f75d2b2116bfed47c5739e9c06f52c99
umashankar <56188006+umashankar14412@users.noreply.github.com>
2020-07-16 19:09:03 +0530

    week 3

---
+++ 5. Java Programming: Build a Recommendation System/Week 3/Assignment 2/Rater.java
@@ -0,0 +1,23 @@
+
+/**
+ * Rater is an interface for PlainRater and EfficientRater classes.
+ * 
+ * @ umashankar
+ * @ Version: 1.0
+ */
+
+import java.util.*;
+
+public interface Rater {
+    public void addRating(String item, double rating);
+
+    public boolean hasRating(String item);
+
+    public String getID();
+
+    public double getRating(String item);
+
+    public int numRatings();
+
+    public ArrayList<String> getItemsRated();
+}
